16f4f9a9d7535c43e3fcd1d1b1d3f610392b66fe
Puszkee <kubalacki1@gmail.com>
2023-01-18 18:18:28 +0100

    skillsfinal

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
     $(".SkillSlide").slideUp();
     $(".Skill").click(function(){
         $(this).children(".SkillSlide").slideToggle("slow");
-      });  
+      }); 
+    $(".SkillSlide").scroll(function() {
+        $(".SkillSlide").slideDown("slow");
+    }); 
 }); 
