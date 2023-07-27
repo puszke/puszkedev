39658b79ecd0f2dc8514d96eaf30820ebafa9149
Puszkee <kubalacki1@gmail.com>
2023-07-27 17:18:57 +0200

    b

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var back = 0;
+
 $(document).ready(function(){
     $(".SkillSlide").slideUp();
     $(".Skill").click(function(){
@@ -5,5 +7,30 @@ $(document).ready(function(){
       }); 
     $(".SkillSlide").scroll(function() {
         $(".SkillSlide").slideDown("fast");
-    }); 
+    });
+    const interval = setInterval(function() {
+        ChangeScreen();
+    }, 8000);
 }); 
+function resolveAfterXSeconds(x) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, x*1000);
+    });
+  }
+async function ChangeScreen()
+{
+    let backs = ["game1.png","game3.png","game2.png"];
+    $(".AboutBackground").addClass("FadeAway");
+    await resolveAfterXSeconds(1);
+    $(".AboutBackground").addClass("Show");
+    $(".AboutBackground").removeClass("FadeAway");
+    $(".AboutBackground").css("background-image","url('"+backs[back]+"')");
+    await resolveAfterXSeconds(1);
+    $(".AboutBackground").removeClass("Show");
+    back++;
+
+    if(back>backs.length-1)
+        back=0;
+}
